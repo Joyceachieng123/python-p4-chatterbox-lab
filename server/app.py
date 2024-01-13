a62f51e005e8eaa2411ff951ee8b158e9603e8fd
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2024-01-13 12:55:49 +0300

    Complete Assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from flask import jsonify
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +14,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    if not messages:
+        return jsonify([])  # Return an empty list if there are no messages
+
+    return jsonify([message.to_dict() for message in messages])
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    response = jsonify(new_message.to_dict())
+    response.headers['Content-Type'] = 'application/json'  # Ensure this line is present
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    response = jsonify(message.to_dict())
+    response.headers['Content-Type'] = 'application/json'  # Ensure this line is present
+    return response
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first_or_404()
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": "Message deleted successfully"})
 @app.route('/messages')
 def messages():
+    # Additional route logic for /messages
     return ''
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
+    # Additional route logic for /messages/<int:id>
     return ''
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
